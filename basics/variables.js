7e14708bf4ed4ed728b8e8784343b0b8eed54c15
AkashMw <60785148+AkashMw@users.noreply.github.com>
2024-04-09 11:21:03 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 14453
+let accountEmail = "akash@gmail.com"
+var accountPassword = "123456"
+accountAddress= "Indore"
+
+let accountState;
+
+
+//accountId = 2
+/*
+var create issues with scopes
+*/
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountState]);
