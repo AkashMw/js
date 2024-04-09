31d7d2f7975eb641692d71c1f2ccbd0f178e6732
AkashMw <60785148+AkashMw@users.noreply.github.com>
2024-04-09 12:10:41 +0000

    datatype_summary

---
+++ basics/datatype_summary.js
@@ -0,0 +1,31 @@
+// primitive
+
+//7 types => String, Number, Boolean, null, undefiend, symbol, BigInt
+
+
+//Reference(Non primitive)
+
+//Array, Object, Functions
+
+
+const score = 3
+const isLoggedIn = true
+
+
+let userEmail;
+
+let id = Symbol('123')
+let anotherId = Symbol('123')
+
+const heros = ["shaktiman", "naagraj", "daga"]
+
+let myObj = {
+    name: "akash",
+    age: 23
+}
+
+const myFunction = function(){
+    console.log("helo");
+}
+
+console.log(typeof(myFunction));
