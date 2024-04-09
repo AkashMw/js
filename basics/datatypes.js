4e270907db7ce06a8ce8cb91d543fd4f0ea0d965
AkashMw <60785148+AkashMw@users.noreply.github.com>
2024-04-09 11:20:50 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,15 @@
+"use scrict"; // treat all JS code as newer version
+
+//alert("hello"); different syntex in node
+
+//number => 2^52
+//bigint
+//string => ""
+//boolean => true or false
+//null = empty or it is a object
+//undefiend = value not assigned or it is a type 
+//symbol 
+
+
+//object
+console.log(typeof address)
