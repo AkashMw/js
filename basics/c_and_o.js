024a8ddc6679c16ef4a098d666d3d62529242de0
AkashMw <60785148+AkashMw@users.noreply.github.com>
2024-04-09 11:33:33 +0000

    conversion

---
+++ basics/c_and_o.js
@@ -0,0 +1,14 @@
+let score = "33"
+
+let valueInNumber = Number(score)
+console.log(typeof(score))
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" => 33
+//"33asd" = NaN
+//true = 1
+
+//Boolean(value)
+//Number(value)
+//String(vales)
