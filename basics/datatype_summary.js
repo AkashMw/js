27ba0a5b13a73fa10ee45a23c9507e62a2879012
AkashMw <60785148+AkashMw@users.noreply.github.com>
2024-04-16 12:33:37 +0000

    maths and numbers

--- basics/datatype_summary.js
+++ basics/datatype_summary.js
@@ -29,3 +29,30 @@ const myFunction = function(){
 }
 
 console.log(typeof(myFunction));
+
+
+// strings 
+
+const   name = "akash"
+const   repocount = 50 
+
+console.log(`hello ${name} and my repo is ${repocount}`)
+
+
+const gameName = new String('hitesh')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+
+console.log(gameName.indexOf('t'));
+
+
+const newString = gameName.substring(0, 4);
+
+const newString1 = gamename.slice(-8, 4);
+console.log(newString);
+console.log(newString1);
---
+++ basics/nums_and_math.js
@@ -0,0 +1,40 @@
+const num = 400;
+const score = new Number(400)
+
+console.log(num);
+
+console.log(score);
+
+
+const  otherNumber = 30.34567654345
+
+console.log(otherNumber.toFixed(2));
+
+console.log(otherNumber.toPrecision(2));
+
+
+
+//--------------------------------------------------------
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.6));
+
+console.log(Math.ceil(3.4));
+
+console.log(Math.random());
+
+console.log(Math.random()*10 + 1);
+
+
+
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() *(max -min + 1))+ min);
+
+
