034f3b746bfe92945170323cc50a837d2ed78bda
AkashMw <60785148+AkashMw@users.noreply.github.com>
2024-04-19 12:30:25 +0000

    functions

--- functions/functions_basics.js
+++ functions/functions_basics.js
@@ -1,1 +1,35 @@
+// function first(){
+//     console.log("A");
+//     console.log("k");
+//     console.log("a");
+//     console.log("s");
+//     console.log("h");
+// }
+// first()
+
+
+// const result = function(num1, num2){
+//     return num1+ num2
+// }
+
+// console.log(result(2, 3));
+
+
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(100, 200, 300));
+
+
+const user = {
+    name: "akash",
+    price: 2345
+}
+
+const handleObject = function(obj){
+    console.log(`${obj.name} hello`);
+}
+user.name = "kirti"
+handleObject(user);
 
